401228d51e47a2f1db96e8e210c0ccdb623dfb1c
Munirih Nganyade <linkmunirih@gmail.com>
2026-09-14 16:24:01 +0200

    Edit product controller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,3 +1,24 @@
 const Joi = require('joi');
 const ProductModel = require('../models/product.model')
 
+
+
+
+
+const deleteProductById = async (req, res, next) => {
+    try{
+        const product = await ProductModel.findByIdAndDelete(req.params.id)
+        if(!product) {
+            return res.status(404).json({
+                message: `Product with id ${req.params.id} not found`
+            })
+        }
+        return res.status(200).json({
+            message: "Product deleted successfully!"
+        })
+    }
+    catch (error) {
+        console.log(error)
+        next(error)
+    }  
+}
